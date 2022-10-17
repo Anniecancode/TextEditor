c1ffc8c148aea8adc9fc308d2a8e18f3e16f38da
Anniecancode <99180884+Anniecancode@users.noreply.github.com>
2022-10-17 23:39:56 +1030

    added src-sw and gitignore, fixed some bugs

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,10 +18,10 @@ export const putDb = async (content) => {
   const jateDB = await openDB('jate', 1);
   const tx = jateDB.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ jate: content });
+  const request = store.put({ id: 1, value: content });
   const result = await request;
   console.log('🚀 - data saved to the database', result);
-  return result;
+  //return result;
 }
 //console.error('putDb not implemented');
 
@@ -31,10 +31,11 @@ export const getDb = async () => {
   const jateDB = await openDB('jate', 1);
   const tx = jateDB.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const request = store.getAll();
+  //const request = store.getAll();
+  const request = store.get(1);
   const result = await request;
-  console.log('request.value', result);
-  return result;
+  console.log('result.value', result);
+  return result.value;
 }
 //console.error('getDb not implemented');
 
