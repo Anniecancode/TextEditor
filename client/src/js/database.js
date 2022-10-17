a7e369257fa0464d9f2e74c284d5d08be18270da
Anniecancode <99180884+Anniecancode@users.noreply.github.com>
2022-10-17 23:54:32 +1030

    fixed indexDB bug

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -35,7 +35,7 @@ export const getDb = async () => {
   const request = store.get(1);
   const result = await request;
   console.log('result.value', result);
-  return result.value;
+  return result?.value;
 }
 //console.error('getDb not implemented');
 
